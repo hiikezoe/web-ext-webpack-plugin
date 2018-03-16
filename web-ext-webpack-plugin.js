b372bf6c8d741ec4be881878e5fc264d24e4c66c
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-03-17 08:27:49 +0900

    A webpack plugin to do web-ext lint

---
+++ web-ext-webpack-plugin.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const webExt = require('web-ext').default;
+
+const pluginName = 'WebExtWebpackPlugin';
+
+class WebExtWebpackPlugin {
+  constructor() {
+  }
+
+  apply(compiler) {
+    const sourceDir = process.cwd();
+    const artifactsDir = path.join(process.cwd(), 'web-ext-artifacts');
+
+    const done = async () => {
+      return webExt.cmd.lint({
+        artifactsDir,
+        boring: false,
+        metadata: false,
+        output: 'text',
+        pretty: false,
+        sourceDir,
+        verbose: false,
+        warningsAsErrors: true,
+      }, {
+        shouldExitProgram: false,
+      });
+    }
+
+    if (compiler.hooks) {
+      compiler.hooks.done.tapPromise({ name: pluginName }, done);
+    } else {
+      compiler.plugin('done', done);
+    }
+  }
+}
+
+module.exports = WebExtWebpackPlugin;
