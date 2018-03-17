3b36a159e658f6716f8e863866f05700c030f6c0
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-03-17 11:14:45 +0900

    Don't run lint if there is any errors

--- web-ext-webpack-plugin.js
+++ web-ext-webpack-plugin.js
@@ -13,7 +13,10 @@ class WebExtWebpackPlugin {
     const sourceDir = process.cwd();
     const artifactsDir = path.join(process.cwd(), 'web-ext-artifacts');
 
-    const done = async () => {
+    const done = async (stats) => {
+      if (stats.hasErrors()) {
+        return;
+      }
       return webExt.cmd.lint({
         artifactsDir,
         boring: false,
