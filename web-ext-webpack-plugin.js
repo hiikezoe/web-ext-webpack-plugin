2685ab46eac5432be58d473a1c0440e6988b3212
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-03-19 06:46:48 +0900

    Launch Firefox only on watch mode

--- web-ext-webpack-plugin.js
+++ web-ext-webpack-plugin.js
@@ -8,11 +8,16 @@ const pluginName = 'WebExtWebpackPlugin';
 class WebExtWebpackPlugin {
   constructor() {
     this.runner = null;
+    this.watchMode = false;
   }
 
   apply(compiler) {
     const sourceDir = process.cwd();
     const artifactsDir = path.join(sourceDir, 'web-ext-artifacts');
+
+    const watchRun = async (compiler) => {
+      this.watchMode = true;
+    };
 
     const afterEmit = async (compilation) => {
       try {
@@ -28,6 +33,10 @@ class WebExtWebpackPlugin {
         }, {
           shouldExitProgram: false,
         });
+
+        if (!this.watchMode) {
+          return;
+        }
 
         if (this.runner) {
           this.runner.reloadAllExtensions();
@@ -54,8 +63,10 @@ class WebExtWebpackPlugin {
 
     if (compiler.hooks) {
       compiler.hooks.afterEmit.tapPromise({ name: pluginName }, afterEmit);
+      compiler.hooks.watchRun.tapPromise({ name: pluginName }, watchRun);
     } else {
       compiler.plugin('afterEmit', afterEmit);
+      compiler.plugin('watchRun', watchRun);
     }
   }
 }
